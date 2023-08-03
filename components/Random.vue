a7236120a041ad0c8d7efa455717e254f078a134
arka1002 <arka.mcu700@gmail.com>
2023-08-03 20:28:06 +0530

    finalized the svg

--- components/Random.vue
+++ components/Random.vue
@@ -1,7 +1,7 @@
 <template>
-    <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <svg width="100" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
 
-        <polyline points="60 110 65 120 70 115 75 130 80 125 85 140 90 135 95 150 100 145" stroke="orange"
-            fill="transparent" stroke-width="5" />
+        <polyline points="0 0 50 0 50 125 100 125 50 125 50 250 0 250" stroke="orange" fill="transparent"
+            stroke-width="5" />
     </svg>
 </template>
