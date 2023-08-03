1aace9b39b2260126e9a7a9d9a3d4ba2f6f02283
arka1002 <arka.mcu700@gmail.com>
2023-08-03 14:26:05 +0530

    initial commit

---
+++ components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-bind:class="classes.cont">{{ data.name }}</div>
+</template>
+
+
+<script setup>
+
+const props = defineProps(["data"])
+
+</script>
+
+
+<style module="classes">
+
+.cont {
+    width: 145px;
+    color: brown;
+    text-align: center;
+    padding: 20px 0px;
+    border: 2px solid black;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <VueFlow v-model="elements" :style="{ backgroundColor: 'lightblue' }">
+            <template #node-custom="props">
+                <Card v-bind="props" />
+            </template>
+        </VueFlow>
+    </div>
+</template>
+
+
+<script setup>
+import { VueFlow } from '@vue-flow/core'
+import { ref } from 'vue'
+import Card from "../components/Card.vue"
+
+
+const elements = ref([
+    {
+        id: '1',
+        type: 'custom',
+        data: { name: "QF1" },
+        position: { x: 0, y: 0 },
+    },
+    {
+        id: '2',
+        type: 'custom',
+        data: { name: "QF2" },
+        position: { x: 100, y: 0 },
+    },
+    {
+        id: 'e1-2',
+        source: '1',
+        target: '2',
+    }
+])
+
+</script>
